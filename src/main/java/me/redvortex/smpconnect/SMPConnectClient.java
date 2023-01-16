aab81b44488eeb54f20ba10fc73f431fd4b0bf29
RedCommand-dev <58099979+RedVortexDev@users.noreply.github.com>
2023-01-16 17:50:19 +0200

    💚🚧 Fix build and basic client

---
+++ src/main/java/me/redvortex/smpconnect/SMPConnectClient.java
@@ -0,0 +1,10 @@
+package me.redvortex.smpconnect;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SMPConnectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
